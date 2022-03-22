ea13cfd66033b276b6fd0ef77b6e508703bb9fa9
Daniel Pellanda <maurizio.pellanda@studio.unibo.it>
2022-03-22 17:32:04 +0100

    Renamed KeyboardHandler class into KeyHandler

---
+++ src/game/utility/input/keyboard/KeyHandler.java
@@ -0,0 +1,34 @@
+package game.utility.input.keyboard;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener{
+
+	public boolean spacePressed;
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		switch(e.getKeyCode()) {
+			case KeyEvent.VK_SPACE:
+				spacePressed = true;
+				break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		switch(e.getKeyCode()) {
+			case KeyEvent.VK_SPACE:
+				spacePressed = false;
+				break;
+		}
+	}
+	
+}
