8b7ed3d14c2f6ef621bf6042461760ca22c0cc30
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2022-04-07 13:02:39 +0200

    slight change of the menu screen

--- src/game/display/DisplayMainMenu.java
+++ src/game/display/DisplayMainMenu.java
@@ -16,9 +16,11 @@ public class DisplayMainMenu implements Display {
 	@Override
 	public void drawScreen(Graphics2D g) {
 		String title = "JetScape";
-		g.setFont(g.getFont().deriveFont(Font.BOLD, 96F));
-		g.drawString(title,this.getCenteredX(g, title),gScreen.getTileSize()*3);
-
+		String msg1 = "Press \"enter\" to start";
+		g.setFont(g.getFont().deriveFont(Font.LAYOUT_RIGHT_TO_LEFT, 110F));
+		g.drawString(title,this.getCenteredX(g, title),gScreen.getTileSize()*2);
+		g.setFont(g.getFont().deriveFont(Font.ITALIC, 35F));
+		g.drawString(msg1,this.getCenteredX(g, msg1),gScreen.getTileSize()*7);
 	}
 	
 	private int getCenteredX(Graphics2D g, String text) {
