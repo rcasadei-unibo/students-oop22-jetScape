1b5c3d01bea61828c27d46d43fa190c68bf6ab92
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2022-04-08 11:42:13 +0200

    added shadows in texts and improved code quality

--- src/game/display/DisplayMainMenu.java
+++ src/game/display/DisplayMainMenu.java
@@ -7,6 +7,12 @@ import java.awt.Graphics2D;
 import game.utility.screen.Screen;
 
 public class DisplayMainMenu implements Display {
+	static final int titleTile = 2;
+	static final int msgTile = 7;
+	static final int titleShift = 5;
+	static final int msgShift = 3;
+	static final Font font = new Font("magneto", Font.PLAIN, 112);
+	
 	private final Screen gScreen;
 
 	public DisplayMainMenu(Screen gScreen) {
@@ -16,13 +22,26 @@ public class DisplayMainMenu implements Display {
 
 	@Override
 	public void drawScreen(Graphics2D g) {
+		int x = 0;
 		String title = "JetScape";
 		String msg1 = "Press \"enter\" to start";
+		
+		//TITLE SHADOW
+		g.setColor(Color.darkGray);
+		g.setFont(font);
+		x = this.getCenteredX(gScreen, g, title);
+		g.drawString(title, x + titleShift, gScreen.getTileSize() * titleTile);
+		//TITLE
 		g.setColor(Color.white);
-		g.setFont(new Font("magneto", Font.PLAIN, 110));
-		g.drawString(title,this.getCenteredX(gScreen, g, title),gScreen.getTileSize()*2);
-		g.setFont(g.getFont().deriveFont(Font.ITALIC, 35F));
-		g.drawString(msg1,this.getCenteredX(gScreen, g, msg1),gScreen.getTileSize()*7);
+		g.drawString(title, x, gScreen.getTileSize() * titleTile);
+		//MESSAGE SHADOW
+		g.setColor(Color.darkGray);
+		g.setFont(g.getFont().deriveFont(Font.ITALIC, 48F));
+		x = this.getCenteredX(gScreen, g, msg1);
+		g.drawString(msg1, x + msgShift, gScreen.getTileSize() * msgTile);
+		//MESSAGE
+		g.setColor(Color.white);
+		g.drawString(msg1, x, gScreen.getTileSize() * msgTile);
 	}
 	
 	
--- src/game/display/DisplayPause.java
+++ src/game/display/DisplayPause.java
@@ -7,6 +7,11 @@ import java.awt.Color;
 import game.utility.screen.Screen;
 
 public class DisplayPause implements Display {
+	static final int msg1Tile = 2;
+	static final int msg2Tile = 7;
+	static final int msg1Shift = 5;
+	static final int msg2Shift = 3;
+	static final Font font = new Font("Stencil",Font.PLAIN,80);
 	private final Screen gScreen;
 
 	public DisplayPause(Screen gScreen) {
@@ -16,14 +21,26 @@ public class DisplayPause implements Display {
 
 	@Override
 	public void drawScreen(Graphics2D g) {
+		int x = 0;
 		String msg1 = "PAUSED";
 		String msg2 = "Press \"r\" to resume , \"e\" return to menu";
-		g.setColor(Color.white);
-		g.setFont(new Font("Stencil", Font.PLAIN, 80));
-		g.drawString(msg1,this.getCenteredX(gScreen, g, msg1),gScreen.getTileSize()*2);
-		g.setFont(g.getFont().deriveFont(Font.ITALIC, 35F));
-		g.drawString(msg2,this.getCenteredX(gScreen, g, msg2),gScreen.getTileSize()*7);
-
+		
+		//MESSAGE 1 SHADOW
+		g.setColor(Color.black);
+		g.setFont(font);
+		x = this.getCenteredX(gScreen, g, msg1);
+		g.drawString(msg1, x + msg1Shift, gScreen.getTileSize() * msg1Tile);
+		//MESSAGE 1
+		g.setColor(Color.lightGray);
+		g.drawString(msg1, x , gScreen.getTileSize() * msg1Tile);
+		//MESSAGE 2 SHADOW
+		g.setColor(Color.black);
+		g.setFont(g.getFont().deriveFont(Font.ITALIC, 32F));
+		x = this.getCenteredX(gScreen, g, msg2);
+		g.drawString(msg2, x + msg2Shift, gScreen.getTileSize() * msg2Tile);
+		//MESSAGE 2
+		g.setColor(Color.lightGray);
+		g.drawString(msg2, x, gScreen.getTileSize() * msg2Tile);
 	}
 
 }
