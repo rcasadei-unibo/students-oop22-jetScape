17448fdf0d36c67c4e43a0e4d93c372e50eb3dc7
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2022-05-04 16:07:50 +0200

    added enumeration for menu options to simplify the code

---
+++ src/game/utility/other/MenuOption.java
@@ -0,0 +1,23 @@
+package game.utility.other;
+
+import java.util.Map;
+
+public enum MenuOption {
+	START, SHOP, QUIT, RECORDS, MENU, RETRY, RESUME;
+	
+	static final Map<MenuOption,GameState> options = Map.of(START, GameState.INGAME,
+			SHOP, GameState.MENU, QUIT, GameState.EXIT, RECORDS, GameState.RECORDS,
+			MENU, GameState.MENU, RETRY, GameState.INGAME, RESUME, GameState.INGAME);
+	
+	public GameState getOptionsGS () {
+		return MenuOption.options.get(this);
+	}
+	
+	@Override
+	public String toString() {
+		if(this.equals(MENU)) {
+			return "back to menu";
+		}
+		return super.toString().toLowerCase();		
+	}
+}
