817e50e0cbad039d896b3fe9f6c361e1a7609ca2
Daniel Pellanda <maurizio.pellanda@studio.unibo.it>
2022-05-18 11:36:53 +0200

    Updated javadoc for Missile and MissileInstance

--- src/game/logics/entities/obstacles/missile/Missile.java
+++ src/game/logics/entities/obstacles/missile/Missile.java
@@ -6,7 +6,10 @@ import game.logics.entities.obstacles.generic.Obstacle;
  * An Interface for accessing {@link MissileInstance} methods.
  * 
  * <p>
- * The class {@link MissileInstance} is used for defining missile obstacle in the environment.
+ * The class {@link MissileInstance} is used for defining a missile obstacle in the environment.
+ * 
+ * A missile is a fast projective that can damage the player if hit, when a missile is about to appear
+ * a warning icon will alert you from which direction the missile is coming from.
  * </p>
  */
 public interface Missile extends Obstacle {
