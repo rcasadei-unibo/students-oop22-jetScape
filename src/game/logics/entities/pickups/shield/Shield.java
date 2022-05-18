b3ea3e8b36ff6dcc5dfec718c03d4b02e19adb6d
Daniel Pellanda <maurizio.pellanda@studio.unibo.it>
2022-05-18 11:47:40 +0200

    Fixed CheckStyle warnings for Shield and ShieldInstance

--- src/game/logics/entities/pickups/shield/Shield.java
+++ src/game/logics/entities/pickups/shield/Shield.java
@@ -2,6 +2,16 @@ package game.logics.entities.pickups.shield;
 
 import game.logics.entities.pickups.generic.Pickup;
 
-public interface Shield extends Pickup{
+/**
+ * An Interface for accessing {@link ShieldInstance} methods.
+ * 
+ * <p>
+ * The class {@link ShieldInstance} is used for defining a shield pickup in the environment.
+ * 
+ * A shield is a bonus item that can be picked up by the player for obtaining a one-time
+ * protection from any hit from any obstacle.
+ * </p>
+ */
+public interface Shield extends Pickup {
 
 }
