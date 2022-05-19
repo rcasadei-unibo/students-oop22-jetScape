300d87039433390d15e336870991f8d9350f4855
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2022-05-19 13:00:24 +0200

    added javadoc for FontLoader

--- src/game/utility/fonts/FontLoader.java
+++ src/game/utility/fonts/FontLoader.java
@@ -5,15 +5,13 @@ import java.awt.FontFormatException;
 import java.io.File;
 import java.io.IOException;
 
-// TODO: Add javadoc
 /**
- * Complete me.
+ * {@link FontLoader} class is used to load and gain access to all the game's different fonts.
  */
 public class FontLoader {
     private static final String SEPARATOR = System.getProperty("file.separator");
-    // TODO: Add javadoc
     /**
-     * Complete me.
+     * absolute path of the directory containing all game's fonts.
      */
     public static final String DEFAULT_DIR = System.getProperty("user.dir") + SEPARATOR
             + "res" + SEPARATOR + "game" + SEPARATOR + "fonts" + SEPARATOR;
@@ -22,9 +20,8 @@ public class FontLoader {
     private final Font optionsFont;
     private final Font textFont;
 
-    // TODO: Add javadoc
     /**
-     * Complete me.
+     * Loads and initializes all the game's different {@link Font}.
      */
     public FontLoader() {
         this.debuggerFont = this.loadFont(DEFAULT_DIR + "debuggerFont.otf");
@@ -43,37 +40,29 @@ public class FontLoader {
         return font;
     }
 
-    // TODO: Add javadoc
     /**
-     * Complete me.
-     * @return complete
+     * @return the default debugger's text font
      */
     public Font getDebuggerFont() {
         return debuggerFont;
     }
 
-    // TODO: Add javadoc
     /**
-     * Complete me.
-     * @return complete
+     * @return the default title's font
      */
     public Font getTitleFont() {
         return titleFont;
     }
 
-    // TODO: Add javadoc
     /**
-     * Complete me.
-     * @return complete
+     * @return the default option's font
      */
     public Font getOptionsFont() {
         return optionsFont;
     }
 
-    // TODO: Add javadoc
     /**
-     * Complete me.
-     * @return complete
+     * @return the default text's font
      */
     public Font getTextFont() {
         return textFont;
