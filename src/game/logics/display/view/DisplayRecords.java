4db0e21b66dcd64462d8d90ec75c9b976c226ea6
Davide Zandonella <davide.zandonella@studio.unibo.it>
2022-06-08 16:58:18 +0200

    show money in DisplayRecords

--- src/game/logics/display/view/DisplayRecords.java
+++ src/game/logics/display/view/DisplayRecords.java
@@ -25,8 +25,8 @@ public class DisplayRecords extends Display implements MenuDisplay {
 
     private final Records records;
 
-    private static final List<String> LENGTH_RECORDS = new ArrayList<>();
-    //private static final List<String> MONEY_RECORDS = new ArrayList<>();
+    private static final List<String> LENGTH_RECORDS = new ArrayList<>(Records.getMaxSavedNumberOfRecords());
+    private static final List<String> MONEY_RECORDS = new ArrayList<>(Records.getMaxSavedNumberOfRecords());
 
     /**
      * {@link DisplayRecords} constructor: add options to be shown.
@@ -45,9 +45,14 @@ public class DisplayRecords extends Display implements MenuDisplay {
 
     private void fetch() {
         DisplayRecords.LENGTH_RECORDS.clear();
+        DisplayRecords.MONEY_RECORDS.clear();
 
         this.records.getRecordScores().forEach(x -> {
             DisplayRecords.LENGTH_RECORDS.add(x.toString());
+        });
+
+        this.records.getRecordCoins().forEach(x -> {
+            DisplayRecords.MONEY_RECORDS.add(x.toString());
         });
 
         /*
@@ -101,11 +106,13 @@ public class DisplayRecords extends Display implements MenuDisplay {
                 super.getGameScreen().getTileSize() * 3, 0);
 
         /*final List<String> moneyList = this.listify(DisplayRecords.MONEY_RECORDS);
+         */
+        final List<String> moneyList = DisplayRecords.MONEY_RECORDS;
         for (int i = 0; i < moneyList.size(); i++) {
             super.drawText(g, super.getTextFont(), moneyList.get(i),
                     super.getGameScreen().getTileSize() * 3 + super.getGameScreen().getWidth() / 2,
                     super.getGameScreen().getTileSize() * (3 + i + 1), 0);
-        }*/
+        }
 
         //OPTIONS
         super.drawOptions(g, DisplayRecords.OPTION_TILE);
