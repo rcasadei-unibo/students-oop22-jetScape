3744994e6cdec9c6a33b979ce22cdf000cdb0365
Giacomo Amadio <giacomo.amadio@studio.unibo.it>
2022-06-09 17:08:50 +0200

    added sound files for different events

--- src/game/utility/other/Sound.java
+++ src/game/utility/other/Sound.java
@@ -3,10 +3,15 @@ package game.utility.other;
 import java.util.Map;
 
 public enum Sound {
-    MAIN_THEME, MENU_SELECTION, ZAPPED;
+    MAIN_THEME, MENU_SELECTION, ZAPPED, COIN, SHIELD_UP,
+    SHIELD_DOWN, TELEPORT, MISSILE, JETPACK, MISSILE_WARNING;
 
-    static final Map<Sound, String> FILES = Map.of(MAIN_THEME, "MainTheme.wav",
-            MENU_SELECTION, "MenuSelect.wav", ZAPPED, "Zapped.wav");
+    public static final Map<Sound, String> FILES = Map.of(MAIN_THEME, "MainTheme.wav",
+            MENU_SELECTION, "MenuSelect.wav", ZAPPED, "Zapped.wav",
+            COIN, "Coin.wav", SHIELD_UP, "ShieldUp.wav",
+            TELEPORT, "Teleport.wav", MISSILE, "Missile.wav",
+            JETPACK, "Jetpack.wav", SHIELD_DOWN,"ShieldDown.wav",
+            MISSILE_WARNING,"MissileWarning.wav");
 
     /**
      *@return the associated track's file name 
