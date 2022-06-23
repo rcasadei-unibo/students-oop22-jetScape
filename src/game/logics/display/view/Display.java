rcasadei-unibo/students-oop22-jetScape

commit a96470c1497ded7d9f6b7734c9d085442882f04b

added javadoc in Display class

src/game/logics/display/view/Display.java
@@ -20,10 +20,13 @@ public abstract class Display {
      */
     private final Screen gScreen = GameWindow.GAME_SCREEN;
 
-    /*
+    /**
      * List of options to be displayed.
      */
     private final List<MenuOption> options;
+    /**
+     * The selected option among {@link #options}.
+     */
     private MenuOption selectedOption;
 
     /**
@@ -58,12 +61,19 @@ public abstract class Display {
     /**
      * Default writing tile for title.
      */
-    private static final int TTITLE_TILE = 2;
+    private static final int TITLE_TILE = 2;
+
     /**
-     * Shadow shifts for options.
+     * Shadow shift for options.
      */
     private static final int OPTIONS_SHIFT = 2;
+    /**
+     * Shadow shift for title.
+     */
     private static final int TITLE_SHIFT = 5;
+    /**
+     * Shadow shift for normal text.
+     */
     private static final int TEXT_SHIFT = 2;
 
     /**
@@ -91,7 +101,9 @@ public abstract class Display {
      */
     private int getCenteredX(final Graphics2D g, final String text) {
 
-        final int lenght = (int) g.getFontMetrics().getStringBounds(text, g).getWidth();
+        final int lenght = (int) g.getFontMetrics()
+                .getStringBounds(text, g)
+                .getWidth();
 
         return gScreen.getWidth() / 2 - lenght / 2;
     }
@@ -120,15 +132,22 @@ public abstract class Display {
         this.selectedOption = selectedOption;
     }
 
-    // TODO: Add javadoc
     /**
-     * .
+     * <p>This method is used to draw some text on screen.
+     * This is the most exhaustive and generic version.</p>
+     * 
+     * <p>The default text color is {@link Display#COLOR}, while the shift
+     * color is implementation-defined as it is passed by
+     * {@link #getShiftColor}.</p>
+     * 
+     * <p>If the {@code shift} parameter is zero the shadow will not be drawn.</p>
+     *
      * @param g the graphics drawer
-     * @param font
-     * @param text
-     * @param xPos
-     * @param yPos
-     * @param shift
+     * @param font the character style of the text
+     * @param text the string with the text that have to be printed on screen
+     * @param xPos the horizontal text starting position (in pixel)
+     * @param yPos the vertical text starting position (in pixel)
+     * @param shift the shift that have to be applied on the text shadow
      */
     protected void drawText(final Graphics2D g, /*final Color color,*/ final Font font,
             final String text, final int xPos, final int yPos, final int shift) {
@@ -144,38 +163,59 @@ public abstract class Display {
         g.drawString(text, xPos, yPos);
     }
 
-    // TODO: Add javadoc
     /**
-     * .
+     * <p>This method is used to draw some centered text on screen.
+     * This is the most exhaustive method.</p>
+     * 
+     * <p>Explanation of how the function parameter works:<ul>
+     * <li>passing to {@code f} a {@link Function#identity}
+     *   will center the text on the axis lying in the middle of the screen</li>
+     * <li>passing {@code x -> x - WIDTH_OF_SCREEN / 4}
+     *   will center the text on the axis corresponding to the first quarter of
+     *   the screen</li>
+     * <li>internally the text is first centered on to the median axis and
+     *   later moved through the function parameter, so x corresponds to the
+     *   value of the abscissa of the median axis.</li>
+     * </ul></p>
+     *
      * @param g the graphics drawer
-     * @param font
-     * @param text
-     * @param f
-     * @param yPos
-     * @param shift
+     * @param font the character style of the text
+     * @param text the string with the text that have to be printed on screen
+     * @param function a function used to calculate the median axis to which the
+     *          text to be written will be aligned
+     * @param yPos the vertical text starting position (in pixel)
+     * @param shift the shift that have to be applied on the text shadow
+     *
+     * @implNote this method calls {@link #drawText(Graphics2D g, Font font, String text, int xCalculated, int yPos, int shift)}
      */
     protected void drawCenteredText(final Graphics2D g, /*final Color color,*/ final Font font,
-            final String text, final Function<Integer, Integer> f, final int yPos, final int shift) {
-        g.setFont(font);
-        this.drawText(g, font, text, f.apply(this.getCenteredX(g, text)), yPos, shift);
+            final String text, final Function<Integer, Integer> function, final int yPos, final int shift) {
+        this.drawText(g, font, text, function.apply(this.getCenteredX(g, text)), yPos, shift);
     }
 
-    // TODO: Add javadoc
     /**
-     * .
+     * Draw a title on screen.
+     * This is a specialised version of {@link #drawCenteredText(Graphics2D, Font, String, Function, int, int)}
+     *
      * @param g the graphics drawer
-     * @param text
-     * @param function
+     * @param text the string with the text that have to be printed on screen
+     * @param function a function used to calculate the median axis to which the
+     *          text to be written will be aligned
+     *
+     * @implNote this method calls calls the aforementioned method using {@linkplain #titleFont font} and
+     * {@linkplain Display#TITLE_SHIFT default} values, furthermore yPos is calculated.
      */
     protected void drawTitleText(final Graphics2D g, final String text, final Function<Integer, Integer> function) {
         this.drawCenteredText(g, this.titleFont, text, Function.identity(),
-                gScreen.getTileSize() * Display.TTITLE_TILE, Display.TITLE_SHIFT);
+                gScreen.getTileSize() * Display.TITLE_TILE, Display.TITLE_SHIFT);
     }
 
     /**
      * Draw list of options with custom yTile.
      * @param g the graphics drawer
      * @param yTile custom value that is used to choose the vertical position.
+     * 
+     * @implNote The option shadow shift is {@link Display#OPTIONS_SHIFT}.
      */
     protected void drawOptions(final Graphics2D g, final int yTile) {
         int i = 0;
@@ -197,6 +237,8 @@ public abstract class Display {
     /**
      * Draw list of options with default yTile.
      * @param g the graphics drawer
+     * @implNote this method calls {@link #drawOptions(Graphics2D, int)} with
+     * {@link Display#OPTION_TILE}.
      */
     protected void drawOptions(final Graphics2D g) {
         this.drawOptions(g, Display.OPTION_TILE);
@@ -205,7 +247,7 @@ public abstract class Display {
     /**
      * Calculate scaled font size.
      *
-     * @param scale
+     * @param scale the scale inverse coefficient
      * @return font's scaled size based on screen height
      */
     protected float getScaledSize(final double scale) {
@@ -216,13 +258,13 @@ public abstract class Display {
      * Abstract method that is used to get chosen shift {@link Color} from
      * the child classes.
      *
-     * @return chosen shift {@link Color}, usually {@code Color.DARK_GRAY}
+     * @return chosen shift {@link Color}, usually {@link Color#DARK_GRAY}
      */
     protected abstract Color getShiftColor();
 
     /**
      * Get standard title font.
-     * @return {@link Display#titleFont}
+     * @return {@link #titleFont}
      */
     protected Font getTitleFont() {
         return this.titleFont;
@@ -230,7 +272,7 @@ public abstract class Display {
 
     /**
      * Get standard options font.
-     * @return Display.optionsFont
+     * @return {@link #optionsFont}
      */
     protected Font getOptionsFont() {
         return this.optionsFont;
@@ -238,7 +280,7 @@ public abstract class Display {
 
     /**
      * Get standard text font.
-     * @return Display.textFont
+     * @return {@link #textFont}
      */
     protected Font getTextFont() {
         return this.textFont;
@@ -246,7 +288,7 @@ public abstract class Display {
 
     /**
      * Get standard text shift.
-     * @return Display.textShift
+     * @return {@link Display#TEXT_SHIFT}
      */
     protected int getTextShift() {
         return Display.TEXT_SHIFT;
@@ -254,7 +296,7 @@ public abstract class Display {
 
     /**
      * Get standard option shift.
-     * @return Display.OPTIONS_SHIFT
+     * @return {@link Display#OPTIONS_SHIFT}
      */
     protected int getOptionShift() {
         return Display.OPTIONS_SHIFT;
@@ -262,7 +304,7 @@ public abstract class Display {
 
     /**
      * Get current game screen.
-     * @return {@link Display#gScreen}
+     * @return {@link #gScreen}
      */
     protected Screen getGameScreen() {
        return gScreen;

commit ea10548a331695676f71b5120e707429caf232ab

bug fix: horizontal text erroneous position, bug introduced in commit a96470c

src/game/logics/display/view/Display.java
@@ -190,6 +190,7 @@ public abstract class Display {
      */
     protected void drawCenteredText(final Graphics2D g, /*final Color color,*/ final Font font,
             final String text, final UnaryOperator<Integer> function, final int yPos, final int shift) {
+        g.setFont(font); // this has to be called before this.getCenteredX
         this.drawText(g, font, text, function.apply(this.getCenteredX(g, text)), yPos, shift);
     }
 