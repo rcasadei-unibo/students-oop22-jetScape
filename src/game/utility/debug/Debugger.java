c3a868c451ef8c9ff8859b3f19e90fbb243e6eff
Daniel Pellanda <maurizio.pellanda@studio.unibo.it>
2022-06-27 21:29:30 +0200

    Added debug option for background coordinates

--- src/game/utility/debug/Debugger.java
+++ src/game/utility/debug/Debugger.java
@@ -29,7 +29,7 @@ public class Debugger {
     /**
      * Enumerable used to identify each debug feature.
      */
-    public enum Option { FPS_METER, DRAW_TIME, ENTITY_COORDINATES, NEXT_SPAWN_TIMER, DIFFICULTY_LEVEL, HITBOX, LOG_FPS, LOG_CLEAN, LOG_SPAWN, LOG_HITCHECK };
+    public enum Option { FPS_METER, DRAW_TIME, ENTITY_COORDINATES, BACKGROUND_COORDINATES, NEXT_SPAWN_TIMER, DIFFICULTY_LEVEL, HITBOX, LOG_FPS, LOG_CLEAN, LOG_SPAWN, LOG_HITCHECK };
 
     /**
      * A map of flags that tells whether a certain debug function (specified as a key)
@@ -56,6 +56,7 @@ public class Debugger {
         optionEnabled.put(Option.FPS_METER, true);
         optionEnabled.put(Option.DRAW_TIME, true);
         optionEnabled.put(Option.ENTITY_COORDINATES, true);
+        optionEnabled.put(Option.BACKGROUND_COORDINATES, false);
         optionEnabled.put(Option.NEXT_SPAWN_TIMER, true);
         optionEnabled.put(Option.DIFFICULTY_LEVEL, true);
         optionEnabled.put(Option.HITBOX, true);
