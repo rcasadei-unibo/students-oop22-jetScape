d182659f30c7a221e194d28976e71feb47d46ae7
Daniel Pellanda <maurizio.pellanda@studio.unibo.it>
2022-06-29 15:54:43 +0200

    Added javadoc for FormatException

--- src/game/utility/other/FormatException.java
+++ src/game/utility/other/FormatException.java
@@ -1,15 +1,17 @@
 package game.utility.other;
 
 /**
- * .
+ * The class {@link FormatException} describes an exception occured when
+ * a bad formatted file is read and its contents cannot be turned into an object.
  */
 public class FormatException  extends Exception {
 
     private static final long serialVersionUID = 1L;
 
     /**
-     * .
-     * @param message
+     * Constructs a new {@link FormatException} containing the specified message.
+     * 
+     * @param message a string describing the cause of the error.
      */
     public FormatException(final String message) {
         super(message);
